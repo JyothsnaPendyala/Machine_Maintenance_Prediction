c2593baa545d86a420231a4ecd756d5970830664
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-08-28 18:02:54 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -13,6 +13,7 @@ def feature_engineering():
     dataset_balanced = pd.concat([Target_1_over,Target_0], axis=0)
     dataset_balanced['Target'].groupby(dataset_balanced['Target']).count()
     data = dataset_balanced.copy()
+    data = data.reset_index(drop=True)
     data.to_csv("machine_predictive_maintenance_clean_data.csv", index = False)
     return data
 
