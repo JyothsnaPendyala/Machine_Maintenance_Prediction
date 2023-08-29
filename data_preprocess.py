491d3a0df704c87df87ed8c73e703c0f47e7c0e5
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-08-29 17:09:29 +0530

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,6 +3,7 @@ from load_data import load_data
 def data_preprocess():
     dataset = load_data()
     dataset.drop(['UDI','Product ID','Failure Type'], axis = 1, inplace = True)
+    dataset.columns = dataset.columns.str.replace(' ', '_') 
     print(dataset.head())
     return dataset
 
